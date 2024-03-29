55370f648609174fc6f57eb54bf3581afc08d169
Vincius-Drums <anaogeraldino@gmail.com>
2024-03-29 18:33:59 -0300

    [M2S05] Ex 02 - Mini-projeto - Cadastro de Heróis (estrutura)

---
+++ src/br/senai/devtubaraosc/heroi/Principal.java
@@ -0,0 +1,12 @@
+package br.senai.devtubaraosc.heroi;
+
+public class Principal {
+    // Método main
+    public static void main(String[] args) {
+        // Criando uma instância da classe de aplicação (DevHeroiAplicacao)
+        DevHeroiAplicacao app = new DevHeroiAplicacao();
+        
+        // Invocando o método 'executar' para deixar a aplicação rodando
+        app.executar();
+    }
+}
---
+++ src/br/senai/devtubaraosc/heroi/enuns/Operacao.java
@@ -0,0 +1,7 @@
+package br.senai.devtubaraosc.heroi.enuns;
+
+public enum Operacao {
+    ADICIONAR,
+    LISTAR,
+    SAIR
+}
